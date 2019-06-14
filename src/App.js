7511ca78dc956f678fc9b4e1445aeafe95bd76ab
aliwo <aliwo@naver.com>
2019-06-14 13:52:28 +0900

    initiall commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './Routes/Home';
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/Components/Router.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from '../Routes/Home';
+
+export default () => {};

b55d33f86b75c8fa9ac17e2b411a608de967c2c0
aliwo <aliwo@naver.com>
2019-06-26 12:41:53 +0900

    블록체인 통신 함수 정교화

--- src/Libs/love.js
+++ src/Libs/love.js
@@ -1,24 +1,45 @@
 /* global web3 */
 
-// axios 를 사용해서 node 서버로부터 최신 contractInfo 를 가져온 후 sendMsg 를 호출해야 된다.
-export const sendMsg = (contractInfo, loveMsg) => {
-    if (!web3) {
-        throw Error('METAMASK_NOT_FOUND');
+// love 를 import 하는 순간에 web3 상태를 확인합니다.
+if (!web3) {
+    throw Error('METAMASK_NOT_FOUND');
+}
+
+web3.eth.getBalance(web3.eth.coinbase, function (error, result) {
+    if(!error) {
+        if (JSON.stringify(result) === '0') {
+            // TODO: 잔고를 어딘가에 저장해 두기.
+            throw Error('NOT_ENOUGH_MINERAL')
+        }
     }
+    else {
+        throw Error('ERROR_LOADING_BALANCE')
+    }
+});
+
+function createContract(contractInfo) {
     const Contract = web3.eth.contract(contractInfo.abi);
     const contract = Contract.at(contractInfo.address);
-    contract.setMessage(loveMsg.url, loveMsg.message, function (err, result) {
-        console.log(result);
+    return contract
+}
+
+// axios 를 사용해서 node 서버로부터 최신 contractInfo 를 가져온 후 sendMsg 를 호출해야 된다.
+export const sendMsg = (contractInfo, loveMsg, afterSend) => {
+    const contract = createContract(contractInfo);
+    contract.setMessage(loveMsg.url, loveMsg.message, function (error, result) {
+        // result 는 transaction 주소이다. 이 transaction 에 콜백을 달아야 함.
+        if (error) {
+            throw Error(JSON.stringify(error))
+        }
+        if (afterSend !== undefined) {
+            afterSend(result);
+        }
     });
 };
 
 export const getMsg = (contractInfo, key) => {
-    if (!web3) {
-        throw Error('METAMASK_NOT_FOUND');
-    }
-    const Contract = web3.eth.contract(contractInfo.abi);
-    const contract = Contract.at(contractInfo.address);
-    contract.getMessage(key, function (err, result) {
+    const contract = createContract(contractInfo);
+    contract.getMessage(key, function (error, result) {
         console.log(result);
     });
 };

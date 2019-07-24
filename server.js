037e5d82496f23d0791ce1588e41cc29556a8a1c
Dorage-San <baloonflower554@gmail.com>
2019-07-24 19:08:42 +0900

    dotenv 선언위치 server.js 로 옮김 / 빌드 변경

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
+import express from 'express';
+import favicon from 'express-favicon';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -8,10 +11,10 @@ app.use(favicon(`${__dirname}/build/favicon.ico`));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function(req, res) {
+app.get('/ping', (req, res) => {
     return res.send('pong');
 });
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import dotenv from 'dotenv';
 import App from './Components/App';
 
-dotenv.config();
 console.log(process.env);
 console.log(process.env.NODE_URL);
 

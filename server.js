952a8bf46d633b66bb40bf662cbc396de1897a47
Dorage-San <baloonflower554@gmail.com>
2019-07-24 19:13:39 +0900

    ES6 => standard

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-import express from 'express';
-import favicon from 'express-favicon';
-import path from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config();
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -11,10 +9,10 @@ app.use(favicon(`${__dirname}/build/favicon.ico`));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', (req, res) => {
+app.get('/ping', function(req, res) {
     return res.send('pong');
 });
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 

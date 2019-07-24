1afdae0272de3daf3a76265d12a55bdbc96e7531
Dorage-San <baloonflower554@gmail.com>
2019-07-24 19:49:23 +0900

    baseURL 변

--- src/api.js
+++ src/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NODE_URL
-        ? `${process.env.NODE_URL}:4000/`
-        : 'http://localhost:4000/'
+    baseURL: process.env.REACT_APP_NODE_URL || 'http://localhost:4000/'
 });
 
 const LoveApi = {

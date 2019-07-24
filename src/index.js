444831299f93fbf7fde305982c8a71ca6a154d7c
Dorage-San <baloonflower554@gmail.com>
2019-07-24 18:41:28 +0900

    .env 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dotenv from 'dotenv';
 import App from './Components/App';
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));

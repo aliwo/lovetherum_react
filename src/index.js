411e6d85639ca632086e8146ce6a2784cdf07413
Dorage-San <baloonflower554@gmail.com>
2019-07-24 18:43:49 +0900

    minor

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import dotenv from 'dotenv';
 import App from './Components/App';
 
 dotenv.config();
+console.log(process.env);
+console.log(process.env.NODE_URL);
 
 ReactDOM.render(<App />, document.getElementById('root'));
